d40f58dae3f0ab081780f7b34417b5996e6bd32a
arturpanteleev <arturpanteleev@gmail.com>
2019-10-22 22:48:26 +0300

    Updates

---
+++ precache-manifest.923911c8ed7805fba4ccaa08ebfddc0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680eada8bc8fa583c95f2cc8bbe4ec5f",
+    "url": "/vkapp/index.html"
+  },
+  {
+    "revision": "7d1b8f74636f881dcc92",
+    "url": "/vkapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "30922ac17d0eb4d6458c",
+    "url": "/vkapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7d1b8f74636f881dcc92",
+    "url": "/vkapp/static/js/2.0ca88e62.chunk.js"
+  },
+  {
+    "revision": "30922ac17d0eb4d6458c",
+    "url": "/vkapp/static/js/main.eb04d28b.chunk.js"
+  },
+  {
+    "revision": "421abc05adf2c533ae73",
+    "url": "/vkapp/static/js/runtime-main.6b834a5a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  }
+]);
